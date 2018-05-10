b9522fc0e80b593ebd7cd751a5857265c7f6f7c1
Faza Zulfika P P <zulffaza@gmail.com>
2018-05-10 14:55:30 +0700

    Initialize project (add maven configuration), add iris model, add cluster model, and add iris dataset

---
+++ src/main/java/com/faza/example/simple/cluster/and/cluster/analysis/java/application/Application.java
@@ -0,0 +1,14 @@
+package com.faza.example.simple.cluster.and.cluster.analysis.java.application;
+
+/**
+ * @author Faza Zulfika P P
+ * @version 1.0.0
+ * @since 10 May 2018
+ */
+
+public class Application {
+
+    public static void main(String[] args) {
+        // TODO run application logic here
+    }
+}
